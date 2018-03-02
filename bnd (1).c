fa9586979908ee043087dcf0021899dea24c47e2
HarishDechiraju <33390861+HarishDechiraju@users.noreply.github.com>
2018-03-02 14:59:33 +0530

    Update bnd (1).c

--- bnd (1).c
+++ bnd (1).c
@@ -4,7 +4,7 @@ int solveSudoku(int sudoku[9][9], int row, int col)
 {
     if(row<9 && col<9)
     {
-        if(sudoku[row][col])
+        if(sudoku[row][col])//if the box is not 0 
         {
             if(col-8) return solveSudoku(sudoku, row, col+1);
             else if(row-8) return solveSudoku(sudoku, row+1, 0);
@@ -12,7 +12,7 @@ int solveSudoku(int sudoku[9][9], int row, int col)
         }
         else
         {
-            for(int i=1; i<10;i++)
+            for(int i=1; i<10;i++)//if box is 0, assign a number and check if it works
             {
                 if(check(sudoku, row, col, i))
                 {
@@ -56,7 +56,7 @@ for(int i=0;i<9;i++)
     {
         for(int i=0; i<9;i++)
         {
-            for(int j=0; j<9;j++) printf("%d ", sudoku[i][j]);
+            for(int j=0; j<9;j++) printf("%d ", sudoku[i][j]);//accept the initial unsolved sudoku with 0 as blank boxes
             printf("\n");
         }
     }
